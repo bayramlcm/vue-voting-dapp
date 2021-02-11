74434570df9ff880577ec29592bd60ac2dd8f2d6
Bayram ALAÇAM <bayramlcm14@gmail.com>
2021-02-12 00:38:06 +0300

    Ropsten ağına taşınması

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import VotingApp from '../dapp/build/contracts/VotingApp.json';
 
 export const contracts = {
     votingApp: {
-        contractAddress: "0xB9066569Dc6bbce35C590bEF5293C2482d208416",
+        contractAddress: "0xAA9139950Dd3ce6B37f4c406A9198Db6DC5a6654",
         abi: VotingApp.abi,
     },
 }
